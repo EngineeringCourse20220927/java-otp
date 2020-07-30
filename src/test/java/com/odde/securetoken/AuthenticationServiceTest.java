0ef9dec93932d4aba175107ef0263c695834be13
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-07-30 13:15:25 +0800

    :wrench: upgrade junit to 5

--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -1,8 +1,8 @@
 package com.odde.securetoken;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AuthenticationServiceTest {
 
